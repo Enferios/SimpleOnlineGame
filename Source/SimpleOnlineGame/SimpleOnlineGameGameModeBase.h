0caafb376a82fd5bb2d3acba5dc0459614f1d63d
Chargos81 <64966431+Chargos81@users.noreply.github.com>
2021-05-30 13:29:10 +0300

    Init

---
+++ Source/SimpleOnlineGame/SimpleOnlineGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SimpleOnlineGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLEONLINEGAME_API ASimpleOnlineGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
